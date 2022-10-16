b5969d30cef4af1c6f9ba2f2e0d3c0b5165e03e0
Hassan <h-@outlook.in>
2022-10-17 02:58:17 +0300

    enable auth

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Register User
+     * @param Request $request
+     * @return User
+     */
+    public function register(Request $request)
+    {
+        try{
+            $validate = Validator::make($request->all(),
+            [
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required',
+                'phone' => 'required|integer|digits:8|unique:users,phone'
+            ]);
+
+            if($validate->fails())
+                return response()->json([
+                    'message' => 'Validation Error',
+                    'errors' => $validate->errors()
+                ], 400);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'phone' => $request->phone
+            ]);
+
+            return response()->json([
+                'user' => $user,
+                'token' => $user->createToken('API_Token')->plainTextToken
+            ]);
+
+        }catch(\Throwable $th){
+            return response()->json([
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Login User
+     * @param Request $request
+     * @return User
+     */
+    public function login(Request $request)
+    {
+        try{
+            $validate = Validator::make($request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+            if($validate->fails())
+            return response()->json([
+                'message' => 'Validation Error',
+                'errors' => $validate->errors()
+            ], 400);
+
+            if(!Auth::attempt($request->only(['email', 'password'])))
+                return response()->json([
+                    'message' => 'Email and Password not match with our system',
+                ], 400);
+
+            $user = User::where('email', $request->email)->first();
+
+
+            return response()->json([
+                'user' => $user,
+                'token' => $user->createToken('API_Token')->plainTextToken
+            ]);
+
+        }catch(\Throwable $th){
+            return response()->json([
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('auth')->controller(AuthController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
 });
